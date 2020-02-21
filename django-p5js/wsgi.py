c31384806960c95bae721f487e1de0589d80ac1b
daisymeal <55197775+daisymeal@users.noreply.github.com>
2020-02-21 16:59:02 +0700

    Add files via upload

---
+++ django-p5js/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for django-p5js project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-p5js.settings')
+
+application = get_wsgi_application()
